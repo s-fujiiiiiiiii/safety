11945ac8fa25c9109ceb97d29fdef055e184406b
fujiiiiiiiii <fko2447019@stu.o-hara.ac.jp>
2026-01-21 09:28:56 +0900

    変更

--- backend/safety/accounts/urls.py
+++ backend/safety/accounts/urls.py
@@ -1,20 +1,21 @@
 from django.urls import path
-from .views import CreateUserView
-from .views import LoginView,UserListView
-from .views import CreateGroupView
-from .views import JoinGroupView
-from .views import GroupListView
-from .views import GroupMemberListView
-from .views import GroupMemberRemoveView
+from .views import (
+    CreateUserView,
+    LoginView,
+    CreateGroupView,
+    JoinGroupView,
+    GroupListView,
+    GroupMemberListView,
+    RemoveGroupMemberView,
+)
 
 urlpatterns = [
     path('create_user/', CreateUserView.as_view(), name='create_user'),
     path('login/', LoginView.as_view(), name='login'),
-    path('user_list/', UserListView.as_view(), name='user_list'),
     path("create_group/", CreateGroupView.as_view()),
     path("join_group/", JoinGroupView.as_view()),
     path("groups/", GroupListView.as_view()),
-    path("group_list/", GroupListView.as_view()),  
+    path("group_list/", GroupListView.as_view()),
     path("group_members/", GroupMemberListView.as_view()),
-    path("group_member_remove/", GroupMemberRemoveView.as_view()),
+    path("remove_group_member/", RemoveGroupMemberView.as_view()),
 ]
